a999f02cae28f278afad30c74b07ef404506e9ad
findus classon <35490318+Findusthehaxor@users.noreply.github.com>
2019-10-22 13:13:28 +0200

    Update main.c

--- source/main.c
+++ source/main.c
@@ -1,16 +1,3 @@
-//   3ds-calc a simple calculator homebrew for the nintendo 3ds
-//   Copyright (C) 2019  findus classon
-
-//   This program is free software: you can redistribute it and/or modify
-//   it under the terms of the GNU General Public License as published by
-//   the Free Software Foundation, either version 3 of the License, or
-//   (at your option) any later version.
-
-//   This program is distributed in the hope that it will be useful,
-//   but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//   GNU General Public License for more details.
-
 #include <3ds.h>
 #include <stdio.h>
 
